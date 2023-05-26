aabb7d7a3501328a6b45da1866962df70ca5e66d
Lifeng Huang <nykfengcode@gmail.com>
2023-05-25 22:43:13 -0400

    setup entity framework for Db connection with sql

---
+++ DataOpsCompanion/Models/RuleUsedHistory.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataOpsCompanion.Models
+{
+    public class RuleUsedHistory
+    {
+        [Key]
+        public int Id { get; set; }
+        public int RuleId { get; set; }
+
+        [Required]
+        public string RuleText { get; set; }
+
+        public string? Site { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public DateTime DateLastUsed { get; set; }
+
+        public int UsageCount { get; set; }
+    }
+}
